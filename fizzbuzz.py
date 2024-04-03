98caad9a493412f43978f534b1d54aafefd7878f
Aliyah Gant <aliyahgant5@gmail.com>
2024-04-03 19:09:01 -0400

    Try #3 lolol

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
-
+#FizzBuzz, OpenSAP Bonus Week 2, CodeYou M1 Week 2 Challenge
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
